f7ee91ca00472d7fe86078f4b2ef92fd6780562b
Matthias Kunnen <matthias.kunnen@gmail.com>
2021-04-01 22:11:48 +0200

    Fix CodeSandbox

--- index.ts
+++ index.ts
@@ -1,3 +1,7 @@
 import 'reflect-metadata';
 
+console.log('%c Decoverto playground', 'font-weight: bold; font-size: 50px;');
+console.log(`Examples can be found in 'src/'. Import an example in index.ts or overwrite it with \
+your own code.`)
+
 import './src/inheritance';
