6c1e824195cb7b7a83a8435d69cdbf847823524c
Matthias Kunnen <matthias.kunnen@gmail.com>
2021-04-01 22:14:09 +0200

    Decrease font size of console.log

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import 'reflect-metadata';
 
-console.log('%c Decoverto playground', 'font-weight: bold; font-size: 50px;');
+console.log('%c Decoverto playground', 'font-weight: bold; font-size: 25px;');
 console.log(`Examples can be found in 'src/'. Import an example in index.ts or overwrite it with \
 your own code.`)
 
