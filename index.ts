dd04bd9be01a921f2fe94e7346d1f84a93bc021d
Matthias Kunnen <matthias.kunnen@gmail.com>
2021-04-02 15:25:46 +0200

    Display browser window for easier error viewing

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import 'reflect-metadata';
 
 console.log('%c Decoverto playground', 'font-weight: bold; font-size: 25px;');
 console.log(`Examples can be found in 'src/'. Import an example in index.ts or overwrite it with \
-your own code.`)
+your own code. Keep the browser window open to view errors.`)
 
 // Uncomment one of these imports or write your own code
 // import './src/basic-class';
